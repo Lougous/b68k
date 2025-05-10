52b8b470c253e197a0a3d4bb3f746b49ae4542ac
Lougous <nicolas.levasseur@free.fr>
2025-05-11 01:21:55 +0200

    cd: assembly of current path and requiered path

--- software/system/bin/sh.c
+++ software/system/bin/sh.c
@@ -15,6 +15,7 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <limits.h>
+#include <errno.h>
 
 #define BUFLEN    128
 #define MAX_ARGS  8
@@ -24,7 +25,7 @@ char *c_args[MAX_ARGS];
 
 char cpath[PATH_MAX+1];
 
-void cut_words (char *cline)
+static void _cut_words (char *cline)
 {
   int argc = 0;
   int in = 0;
@@ -55,14 +56,118 @@ void cut_words (char *cline)
   c_args[argc] = 0;   
 }
 
-char *_symplify_path(char *path)
+static char *_trim_slash(char *str)
 {
-  // TODO
-  return path;
+  while (*str && (*str != '/')) str++;
+  while (*str && (*str == '/')) str++;
+
+  return str;
+}
+
+// len of first path element in name
+static short _subpathlen (char *path)
+{
+  char *at = path;
+
+  while (*at && *at != '/') at++;
+
+  return (short)(at - path);
+}
+
+// build the full path, starting from current directory (relative) or / (absolute)
+//  - resolves . and .. special directories
+//  - resulting path always starts with /
+static short _build_path(char *dst, char *path)
+{
+  if (!path) path = "/";  // TODO: use $HOME
+  char *wptr;
+  short avail;
+
+  if (*path == '/') {
+    // absolute path
+    wptr = dst;
+    // default heading /, will be overwritten by any path element
+    wptr[0] = '/';
+    wptr[1] = 0;
+    avail = PATH_MAX;
+    path = _trim_slash(path);
+  } else {
+    // relative, starts from current directory
+    strcpy(dst, cpath);
+
+    if (strlen(cpath) == 1) {
+      // must be /, will be overwritten by any path element
+      wptr = dst;
+      avail = PATH_MAX;;
+    } else {
+      wptr = dst+strlen(cpath);
+      avail = PATH_MAX-strlen(cpath);
+    }
+  }
+  
+  while (*path) {
+    // apply path elements one by one
+    // cut path element
+    short elen = _subpathlen(path);
+    char cbak = path[elen];
+    path[elen] = 0;
+
+    // special paths . and ..
+    if (path[0] == '.') {
+      if (elen == 1) {
+	// .
+	path[1] = cbak;
+	path++;
+	path = _trim_slash(path);
+	continue;
+      }
+      
+      if (elen == 2 && path[1] == '.') {
+	// ..
+	if (avail < PATH_MAX) {
+	  // not at root yet
+	  while (*--wptr != '/') avail++;
+	  avail++;
+	  
+	  if (avail == PATH_MAX) {
+	    // rewind up to root
+	    wptr[0] = '/';
+	    wptr[1] = 0;
+	  } else {
+	    *wptr = 0;
+	  }
+	}
+	
+	path[2] = cbak;
+	path += 2;
+	path = _trim_slash(path);
+	continue;
+      }
+    }
+
+    // regular path element
+    if (avail < elen+1) {
+      // too long
+      return -ENAMETOOLONG;
+    }
+    
+    *wptr++ = '/';
+    strcpy(wptr, path);
+    wptr += elen;
+    avail -= (elen+1);
+
+    // next path element
+    path[elen] = cbak;
+    path++;
+    path = _trim_slash(path);
+  }
+
+  return 0;
 }
 
 int main (int argc, char *argv[])
 {
+  strcpy(cpath, "/");
 
   while (1) {
     // TODO: use PS1 environment variable
@@ -101,21 +206,27 @@ int main (int argc, char *argv[])
     }
 
     // break down command & arguments
-    cut_words(line);
+    _cut_words(line);
 
     if (c_args[0]) {
       // internal commands
       if (strcmp(c_args[0], "exit") == 0) break;
 
       if (strcmp(c_args[0], "cd") == 0) {
-	char *path = c_args[1] ? _symplify_path(c_args[1]) : "/";  // TODO use $HOME
+	char path[PATH_MAX+1];
+
+	if (_build_path(path, c_args[1]) < 0) {
+	  printf("sh: cd: %s: Path too long\n", c_args[1]);
+	  continue;
+	}
 	
 	if (chdir(path) < 0) {
 	  printf("sh: cd: %s: No such directory\n", path);
-	} else {
-	  strncpy(cpath, path, PATH_MAX);
-	  cpath[PATH_MAX] = 0;
+	  continue;
 	}
+
+	strncpy(cpath, path, PATH_MAX);
+	cpath[PATH_MAX] = 0;  // safe guard
 
 	continue;
       }
